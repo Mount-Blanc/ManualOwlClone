d4a7c38da6711f04baa012da7bf1203ad15e3f52
Mount-Blanc <mountblanc100@gmail.com>
2023-06-12 13:14:29 -0700

    added header component

--- manualclone/src/App.jsx
+++ manualclone/src/App.jsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+     <header>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <span>Manual Owl</span>
+        <input placeholder='Manual Search'/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <ul>
+          <li><a href=''>Home</a></li>
+          <li><a href=''>My Manuals+</a></li>
+          <li><a href=''>Manual Index</a></li>
+          <li><a href=''>Categories</a></li>
+
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     </header>
     </>
   )
 }
